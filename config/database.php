d0477890cd54e0fb201b9bb2ae0798bd103285d7
jvirocel <148781073+jvirocel@users.noreply.github.com>
2025-05-16 09:20:30 +0800

    DB
    Fix DB Connection

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'candon_city';
-$username = 'root';
-$password = '';
+$host = 'localhost:3306';
+$dbname = 'virolwmh_portal';
+$username = 'virolwmh_portal';
+$password = '^@pCd1C#zC5uJ368HK9TNeSYn5gpSqdgNAzgOux^k2O4VbpZh0KVE1!ZFxx^hPBVkZycGV6o1p0Q$D5dfPnIWg&&ALtopcr#dhXQf9GWIROOc65%&zdi@S$dP7ORq4fZ';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
